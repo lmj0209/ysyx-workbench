d68e1c463e844ab8ea26b8de5c69110a87152960
lmj0209 <971933706@qq.com>
2024-06-18 11:33:12 +0800

    1.delete original main.cpp 2. add tb_example.cpp

---
+++ npc/csrc/tb_example.cpp
@@ -0,0 +1,90 @@
+#include <stdlib.h>
+#include <iostream>
+#include <cstdlib>
+#include <stdio.h>
+#include <assert.h>
+#include <verilated.h>
+#include <verilated_vcd_c.h>
+#include "Vexample.h"
+//#include "Valu___024unit.h"
+
+#define MAX_SIM_TIME 20
+#define VERIF_START_TIME 7
+vluint64_t sim_time = 0;
+vluint64_t posedge_cnt = 0;
+
+// void dut_reset (Valu *dut, vluint64_t &sim_time){
+//     dut->rst = 0;
+//     if(sim_time >= 3 && sim_time < 6){
+//         dut->rst = 1;
+//         dut->a_in = 0;
+//         dut->b_in = 0;
+//         dut->op_in = 0;
+//         dut->in_valid = 0;
+//     }
+// }
+
+// void check_out_valid(Valu *dut, vluint64_t &sim_time){
+//     static unsigned char in_valid = 0; //in valid from current cycle
+//     static unsigned char in_valid_d = 0; //delayed in_valid
+//     static unsigned char out_valid_exp = 0; //expected out_valid value
+
+//     if (sim_time >= VERIF_START_TIME) {
+//         out_valid_exp = in_valid_d;
+//         in_valid_d = in_valid;
+//         in_valid = dut->in_valid;
+//         if (out_valid_exp != dut->out_valid) {
+//             std::cout << "ERROR: out_valid mismatch, "
+//                 << "exp: " << (int)(out_valid_exp)
+//                 << " recv: " << (int)(dut->out_valid)
+//                 << " simtime: " << sim_time << std::endl;
+//         }
+//     }
+// }
+
+// void set_rnd_out_valid(Valu *dut, vluint64_t &sim_time){
+//     if (sim_time >= VERIF_START_TIME) {
+//         dut->in_valid = rand() % 2;
+//     }
+// }
+
+int main(int argc, char** argv, char** env) {
+    srand (time(NULL));
+    Verilated::commandArgs(argc, argv);
+    Vexample *dut = new Vexample;
+
+    Verilated::traceEverOn(true);
+    VerilatedVcdC *m_trace = new VerilatedVcdC;
+    dut->trace(m_trace, 5);
+    m_trace->open("waveform.vcd");
+
+    while (sim_time < MAX_SIM_TIME) {
+
+        int a = rand() & 1;
+        int b = rand() & 1;
+        dut->a = a;
+        dut->b = b;
+        dut->eval();
+        printf("a = %d, b = %d, f = %d\n", a, b, dut->f);
+        assert(dut->f == (a ^ b));
+        
+        
+       //dut_reset(dut, sim_time);
+       //dut->clk ^= 1;
+       //dut->eval();
+
+       //if (dut->clk == 1){
+       //    dut->in_valid = 0;
+       //    posedge_cnt++;
+       //    set_rnd_out_valid(dut, sim_time);
+       //    check_out_valid(dut, sim_time);
+       //}
+
+        m_trace->dump(sim_time);
+        sim_time++;
+    }
+
+    m_trace->close();
+    delete dut;
+    exit(EXIT_SUCCESS);
+}
